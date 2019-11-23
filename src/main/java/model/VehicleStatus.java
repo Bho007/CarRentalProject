fb1c238a9cee22e5f746a563fa9329a5c832d5a1
Bhavesh Singhal <bhaveshsinghal007@gmail.com>
2019-11-23 13:53:49 -0800

    "done with getAllVehiclesQuery"

--- src/main/java/model/VehicleStatus.java
+++ src/main/java/model/VehicleStatus.java
@@ -4,5 +4,20 @@ public enum VehicleStatus {
     FOR_RENT,
     FOR_SALE,
     MAINTENANCE,
-    RENTED
+    RENTED;
+
+    public static VehicleStatus toStatus(String input) {
+        switch (input.toLowerCase()) {
+            case "for_rent":
+                return FOR_RENT;
+            case "for_sale":
+                return FOR_SALE;
+            case "maintenance":
+                return MAINTENANCE;
+            case "rented":
+                return RENTED;
+            default:
+                return null;
+        }
+    }
 }
