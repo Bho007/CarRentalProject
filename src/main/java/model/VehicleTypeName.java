8fbff4008cf541d9781d415f25010363edb2da60
Bhavesh Singhal <bhaveshsinghal007@gmail.com>
2019-11-23 12:05:20 -0800

    test1

---
+++ src/main/java/model/VehicleTypeName.java
@@ -0,0 +1,21 @@
+package model;
+
+public enum VehicleTypeName {
+    ECONOMY("Economy"),
+    COMPACT("Compact"),
+    MIDSIZE("Midsize"),
+    STANDARD("Standard"),
+    FULLSIZE("Fullsize"),
+    SUV("SUV"),
+    TRUCK("Truck");
+
+    private String name;
+
+    VehicleTypeName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
